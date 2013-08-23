47b6a4efe4e077737c4c3639f903ed36709c43d9
asaf <afeset@gmail.com>
2013-08-23 17:16:18 +0300

    ui

--- DAL/Test2.py
+++ DAL/Test2.py
@@ -3,3 +3,18 @@ Created on Aug 7, 2013
 
 @author: asaf
 '''
+class Pair:
+    def __init__(self, key, value):
+        self.key=key
+        self.value=value
+    
+    def Bla(self, i):
+        print(6+i)    
+class Test2:
+    def run(self):
+        list=[]
+        list.append(Pair(1,2))
+        list.append(Pair(3,4))
+        
+        return list
+
