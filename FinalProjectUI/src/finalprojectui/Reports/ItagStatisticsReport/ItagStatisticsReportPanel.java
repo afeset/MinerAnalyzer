fad466d43c24f482094e278ce69f10f982b7cff7
asaf <afeset@gmail.com>
2013-08-26 23:14:08 +0300

    UI updates - graph for itag statistics report

--- FinalProjectUI/src/finalprojectui/Reports/ItagStatisticsReport/ItagStatisticsReportPanel.java
+++ FinalProjectUI/src/finalprojectui/Reports/ItagStatisticsReport/ItagStatisticsReportPanel.java
@@ -34,60 +34,6 @@ public class ItagStatisticsReportPanel extends javax.swing.JPanel {
         
     }
     
-    public void setResultsPanel(ItagStatisticsReportReaultsPanel panel)
-    {
-        this.add(panel, BorderLayout.CENTER);
-        this.repaint();
-    }
-    
-    private void initTable()
-    {
-        // This will create the dataset 
-        PieDataset dataset = createDataset();
-        // based on the dataset we create the chart
-        JFreeChart chart = createChart(dataset, "Title");
-        // we put the chart into a panel
-        ChartPanel chartPanel = new ChartPanel(chart);
-        // default size
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        this.add(chartPanel, BorderLayout.CENTER);
-    }
-    /**
-     * Creates a sample dataset 
-     */
-
-    private  PieDataset createDataset() {
-        DefaultPieDataset result = new DefaultPieDataset();
-        result.setValue("Linux", 29);
-        result.setValue("Mac", 20);
-        result.setValue("Windows", 51);
-        return result;
-        
-    }
-    
-    
-/**
-     * Creates a chart
-     */
-
-    private JFreeChart createChart(PieDataset dataset, String title) {
-        
-        JFreeChart chart = ChartFactory.createPieChart3D(title,          // chart title
-            dataset,                // data
-            true,                   // include legend
-            true,
-            false);
-
-        PiePlot3D plot = (PiePlot3D) chart.getPlot();
-        plot.setStartAngle(290);
-        plot.setDirection(Rotation.CLOCKWISE);
-        plot.setForegroundAlpha(0.5f);
-        return chart;
-        
-    }
- 
-
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
